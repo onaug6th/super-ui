5001d8a109abd62e0318630a4f98b06bc8407e61
onaug6th <592986151@qq.com>
2020-05-05 13:22:59 +0800

    [M] 修改组件库引用注释说明

--- src/main.js
+++ src/main.js
@@ -4,14 +4,16 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
-//  全量使用
+/****************  全量使用 ****************/
 // import superUI from "@onaug6th/super-ui";
 // import "@onaug6th/super-ui/lib/index/style.css";
 // Vue.use(superUI);
-//  全量使用
+/****************  全量使用 ****************/
 
 
-//  基础组件使用
+
+
+/****************  基础组件使用 ****************/
 import superUI_base from "@onaug6th/super-ui/lib/base";
 import "@onaug6th/super-ui/lib/base/style.css";
 Vue.use(superUI_base);
@@ -19,8 +21,10 @@ Vue.use(superUI_base);
 //  使用按需引用组件
 import { SDropdown } from "@onaug6th/super-ui";
 Vue.use(SDropdown);
+/****************  基础组件使用 ****************/
 
-//  基础组件使用
+
+
 
 new Vue({
   router,
